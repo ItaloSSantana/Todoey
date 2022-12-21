36644bfeab9a98c959e9997565f14cecbd0f21c8
ItaloSSantana <110575556+ItaloSSantana@users.noreply.github.com>
2022-12-21 12:24:24 -0800

    Add alert to add items

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -1,7 +1,6 @@
 import UIKit
-
 class TodoListViewController: UITableViewController {
-    let item = ["Buy apples", "Buy bananas", "Buy oranges"]
+    var itemList = ["Buy apples", "Buy bananas", "Buy oranges"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,12 +8,12 @@ class TodoListViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return item.count
+        return itemList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = item[indexPath.row]
+        cell.textLabel?.text = itemList[indexPath.row]
         return cell
     }
     
@@ -24,10 +23,27 @@ class TodoListViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-            
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
+    //MARK - ADD NEW ITEMS
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if let item = textfield.text {
+                self.itemList.append(item)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "Create new Item"
+            textfield = alertTextfield
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 	
