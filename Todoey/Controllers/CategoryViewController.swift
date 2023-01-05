90524aa36f6e3deb0ddb6a5e5e39fc0616e0bbe7
ItaloSSantana <110575556+ItaloSSantana@users.noreply.github.com>
2023-01-05 11:28:11 -0800

    Add category list

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,77 @@
+import UIKit
+import CoreData
+class CategoryViewController: UITableViewController {
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategory()
+    }
+    
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()){
+        do {
+            if let safeContext = context {
+                categories = try safeContext.fetch(request)
+            }
+        } catch {
+            print("error fetching data from context")
+        }
+        tableView.reloadData()
+    }
+    
+    func saveCategory() {
+        do {
+            if let safeContext = context {
+                try safeContext.save()
+                print("saving...")
+            }
+        } catch {
+            print("Error saving context")
+        }
+        tableView.reloadData()
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            if let category = textfield.text {
+                if let safeContext = self.context {
+                    let newCategory = Category(context: safeContext)
+                    newCategory.name = category
+                    self.categories.append(newCategory)
+                    self.saveCategory()
+                }
+            }
+        }
+        
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "Create new Item"
+            textfield = alertTextfield
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+//MARK: - TableView Delegates/Data Source
+
+extension CategoryViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+}
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,69 +11,22 @@ class TodoListViewController: UITableViewController {
         searchBar.delegate = self
         loadItems()
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemList.count
-    }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        let item = itemList[indexPath.row]
-        cell.textLabel?.text = item.title
-        cell.accessoryType = item.done ? .checkmark : .none
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        if let safeContext = context{
-//            context?.delete(itemList[indexPath.row])
-//            itemList.remove(at: indexPath.row)
-//        }
-        itemList[indexPath.row].done = !itemList[indexPath.row].done
-        saveItems()
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        var textfield = UITextField()
-
-        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
-
-        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            if let item = textfield.text {
-                if let safeContext = self.context {
-                let newItem = Item(context: safeContext)
-                newItem.title = item
-                newItem.done = false
-                self.itemList.append(newItem)
-                self.saveItems()
-                }
-            }
-            }
-        
-        alert.addTextField { (alertTextfield) in
-            alertTextfield.placeholder = "Create new Item"
-            textfield = alertTextfield
-        }
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-
     func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
-            do {
-                if let safeContext = context {
-           itemList = try safeContext.fetch(request)
-                }
-            } catch {
-                print("error fetching data from context")
+        do {
+            if let safeContext = context {
+                itemList = try safeContext.fetch(request)
             }
-        tableView.reloadData()
+        } catch {
+            print("error fetching data from context")
         }
+        tableView.reloadData()
+    }
     
     func saveItems() {
         do {
             if let safeContext = context {
-            try safeContext.save()
+                try safeContext.save()
                 print("saving...")
             }
         } catch {
@@ -81,8 +34,31 @@ class TodoListViewController: UITableViewController {
         }
         tableView.reloadData()
     }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if let item = textfield.text {
+                if let safeContext = self.context {
+                    let newItem = Item(context: safeContext)
+                    newItem.title = item
+                    newItem.done = false
+                    self.itemList.append(newItem)
+                    self.saveItems()
+                }
+            }
+        }
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "Create new Item"
+            textfield = alertTextfield
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
-
 //MARK: - Search Bar Methods
 
 extension TodoListViewController: UISearchBarDelegate {
@@ -105,4 +81,29 @@ extension TodoListViewController: UISearchBarDelegate {
     }
 }
 
+//MARK: - TableView Delegate/DataSource
 
+extension TodoListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemList[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //        if let safeContext = context{
+        //            context?.delete(itemList[indexPath.row])
+        //            itemList.remove(at: indexPath.row)
+        //        }
+        itemList[indexPath.row].done = !itemList[indexPath.row].done
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
